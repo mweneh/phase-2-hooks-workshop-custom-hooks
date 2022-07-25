7ef6e62285af1aac9bd25d220d9274bc836f844f
David <davidongaroh@gmail.com>
2022-07-25 23:50:00 +0300

    done

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,24 +2,50 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
 /* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
+export function usePokemon(query) {
   /* ✅ this hook should only return one thing: an object with the pokemon data */
-}
+  const [{data, status, errors}, setPokemon] = useState({
+    data: null,
+    errors: null,
+    status: 'idle',
+  });
 
+  useEffect(() => {
+    setPokemon({data:null, status:'pending', errors:null})
+    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+      .then(r => {
+        if (r.ok){
+          return r.json()
+        }else{
+          return r.text().then (err=>{
+            throw err
+          })
+        }
+      })
+      .then(pokemon=>{
+        setPokemon({data:pokemon,status:'fulfilled', errors:null})
+      })
+      .catch(err=>{
+        setPokemon({data:null,status:'rejected', errors:[err]})
+
+      })
+  }, [query]);
+  return {data, status, errors}
+
+}
 function Pokemon({ query }) {
+  const {data:pokemon,status,errors}= usePokemon(query)
+
+  console.log(pokemon, status, errors)
   /* 
    ✅ move the code from the useState and useEffect hooks into the usePokemon hook
    then, call the usePokemon hook to access the pokemon data in this component
   */
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then(r => r.json())
-      .then(setPokemon);
-  }, [query]);
+// const {data:pokemon }=usePokemon(query)
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
-  if (!pokemon) return <h3>Loading...</h3>;
+  if (status === 'rejected') return <h3>Error:{errors[0]}</h3>
+  if (status !=='fulfilled') return <h3>Loading...</h3>;
 
   return (
     <div>
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,34 +1,59 @@
 import { useEffect, useState } from "react";
 
+function getLocalStorageData(key){
+  let stringifiedValue = localStorage.getItem(key)
+  try {
+    stringifiedValue= JSON.parse(stringifiedValue)
+  } catch {}
+  
+  return stringifiedValue
+}
+function setLocalStorageData(key,value){
+  const stringifiedValue = JSON.stringify(value)
+  localStorage.setItem(key,stringifiedValue)
+}
 /* 
   the two parameters for this function are: 
   - key: the key on localStorage where we are saving this data
   - initialValue: the initial value of state
 */
-export function useLocalStorage(key, initialValue) {
+export function useLocalStorage(key, initialValue = null) {
   /* 
     ✅ in this hook, use the useState hook. For the initial value for state:
     use the value saved in localStorage OR the initialValue from the function parameters 
   */
-
+const [state, setState]= useState(getLocalStorageData(key) || initialValue)
   /* 
    ✅ write a useEffect hook 
    in the useEffect, when state is updated, save the state to localStorage
    don't forget the dependencies array!
   */
-  useEffect(() => {});
+  useEffect(() => {
+    setLocalStorageData(key,state)
+  }, [key,state]);
 
+  useEffect(()=>{
+    function handleStorageUpdate(){
+      const value = getLocalStorageData(key)
+   setState(value)
+    }
+    window.addEventListener('storage',handleStorageUpdate )
+ 
+  return function cleanup(){
+    window.addEventListener('storage',handleStorageUpdate )
+  } 
+},[key])
   /* 
    ✅ return the same interface as useState:
    an array with state and a setState function
   */
-  // 👀 return [state, setState]
+  return [state, setState]
 }
 
 function Form() {
   // ✅ after implementing the useLocalStorage hook, replace useState with useLocalStorage
   // don't forget to pass in both arguments (a key and an initialValue)
-  const [name, setName] = useState("");
+  const [name, setName] = useLocalStorage("name",'');
   console.log(name);
 
   return (
